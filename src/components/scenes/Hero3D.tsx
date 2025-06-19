e42c91e3a6a0418eb9958bf2801d4ad538372b8c
sarran528 <sarranm528@gmail.com>
2025-06-19 15:41:54 +0530

    nav bar added

--- src/components/scenes/Hero3D.tsx
+++ src/components/scenes/Hero3D.tsx
@@ -1,6 +1,6 @@
 import React, { Suspense, useRef } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
-import { OrbitControls, Stars, useGLTF, Environment } from '@react-three/drei';
+import { OrbitControls, Stars, useGLTF, Environment, Float } from '@react-three/drei';
 import * as THREE from 'three';
 import SceneLights from './SceneLights';
 
@@ -10,20 +10,30 @@ const Model: React.FC = () => {
   useFrame((state) => {
     if (meshRef.current) {
       meshRef.current.rotation.y = state.clock.getElapsedTime() * 0.3;
+      meshRef.current.rotation.x = Math.sin(state.clock.getElapsedTime() * 0.4) * 0.2;
     }
   });
 
   return (
-    <mesh ref={meshRef}>
-      <torusKnotGeometry args={[2, 0.5, 256, 64]} />
-      <meshPhysicalMaterial
-        color="#4F46E5"
-        roughness={0.3}
-        metalness={0.9}
-        clearcoat={1}
-        clearcoatRoughness={0.1}
-      />
-    </mesh>
+    <Float
+      speed={1.4}
+      rotationIntensity={0.5}
+      floatIntensity={0.5}
+    >
+      <mesh ref={meshRef} scale={0.8}>
+        <torusKnotGeometry args={[2, 0.5, 256, 64]} />
+        <meshPhysicalMaterial
+          color="#4F46E5"
+          roughness={0.2}
+          metalness={0.9}
+          clearcoat={1}
+          clearcoatRoughness={0.1}
+          emissive="#4338CA"
+          emissiveIntensity={0.4}
+          toneMapped={false}
+        />
+      </mesh>
+    </Float>
   );
 };
 
@@ -33,19 +43,21 @@ const Hero3D: React.FC = () => {
       camera={{ position: [0, 0, 8], fov: 45 }}
       dpr={[1, 2]}
       style={{
-        position: 'absolute',
-        top: 0,
-        left: 0,
         width: '100%',
         height: '100%',
         background: 'transparent',
+        borderRadius: 'inherit',
       }}
+      gl={{ antialias: true }}
     >
+      <color attach="background" args={['#000000']} />
+      <fog attach="fog" args={['#000000', 5, 15]} />
+      
       <Suspense fallback={null}>
         <SceneLights />
         <Stars
-          radius={50}
-          depth={50}
+          radius={40}
+          depth={30}
           count={3000}
           factor={4}
           saturation={0}
@@ -53,7 +65,7 @@ const Hero3D: React.FC = () => {
           speed={1}
         />
         <Model />
-        <Environment preset="city" />
+        <Environment preset="night" />
         <OrbitControls
           enableZoom={false}
           enablePan={false}
@@ -62,6 +74,7 @@ const Hero3D: React.FC = () => {
           autoRotateSpeed={0.5}
           minPolarAngle={Math.PI / 2.5}
           maxPolarAngle={Math.PI / 1.5}
+          makeDefault
         />
       </Suspense>
     </Canvas>
--- src/components/scenes/SceneLights.tsx
+++ src/components/scenes/SceneLights.tsx
@@ -3,15 +3,25 @@ import React from 'react';
 const SceneLights: React.FC = () => {
   return (
     <>
-      <ambientLight intensity={0.2} />
+      <ambientLight intensity={0.15} />
       <directionalLight
-        position={[10, 10, 5]}
-        intensity={1}
-        castShadow
-        shadow-mapSize-width={2048}
-        shadow-mapSize-height={2048}
+        position={[5, 5, 3]}
+        intensity={0.8}
+        color="#4F46E5"
       />
-      <pointLight position={[-10, -10, -5]} intensity={0.5} />
+      <pointLight
+        position={[-5, -5, -3]}
+        intensity={0.4}
+        color="#818CF8"
+      />
+      <spotLight
+        position={[3, 3, 3]}
+        angle={0.4}
+        penumbra={1}
+        intensity={0.5}
+        color="#C7D2FE"
+        castShadow
+      />
     </>
   );
 };
